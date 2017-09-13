1b34e3747b561e60798594e4774db5dba16c8eec
calzonelover <patomporn.pay@gmail.com>
2017-09-13 16:58:42 +0700

    good evening

--- RNN/ex_1_ImdbKeras/RNN1_ImdbKeras.py
+++ RNN/ex_1_ImdbKeras/RNN1_ImdbKeras.py
@@ -1,20 +1,17 @@
 import numpy as np
-from keras.datasets import imdb
+from keras.datasets import imdb # review
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 from keras.optimizers import Adam
-# fix random seed for reproducibility
-np.random.seed(7)
+# setting
+batch_size = 64
+max_review_length = 500 # truncate and pad input sequences (clear 0 and use only 500)
 # load the dataset but only keep the top n words, zero the rest
 top_words = 5000 # max word only 5000 words in this dictionary
-(X_train, y_train), (X_test, y_test) = imdb.load_data(nb_words=top_words)
-# truncate and pad input sequences (clear 0 and use only 500)
-print X_train.np.shape
-exit()
-max_review_length = 500
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
 X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
 X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
 # create the model
@@ -24,8 +21,11 @@ model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_l
 model.add(LSTM(100))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.00025), metrics=['accuracy'])
-print(model.summary())
-model.fit(X_train, y_train, nb_epoch=3, batch_size=64)
+print model.summary()
+### test
+print y_train.shape
+###
+model.fit(X_train, y_train, nb_epoch=3, batch_size=batch_size)
 # save model
 model.save('ImdbReview_PN_1.h5')
 # Final evaluation of the model
--- RNN/ex_2_ImageRecognition_Keras/CNN_img_recog.py
+++ RNN/ex_2_ImageRecognition_Keras/CNN_img_recog.py
@@ -1,3 +1,50 @@
 import numpy as np
 from keras.datasets import mnist # data set of number
+from keras.models import Sequential
+from keras.layers import Dense,Conv2D,Dropout,Flatten
+from keras.layers import LSTM,convolutional,MaxPooling2D
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+from keras.optimizers import Adam
+
+import matplotlib.pyplot as plt
+
+# setting
+batch_size = 128
+N_class = 10
+nb_epoch = 3
+name_saved_model = 'CNN+LSTM_on_MNIST.h5'
+# model
+def model(x_train):
+    model=Sequential()
+    model.add(Conv2D(32,(4,4),activation='relu',input_shape=(x_train[0].shape[0],x_train[0].shape[1],1)))
+    model.add(MaxPooling2D(pool_size=(3,3)))
+    model.add(Conv2D(32,(3,3),activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.2))
+    model.add(Flatten())
+    model.add(Dense(128,activation='relu'))
+    model.add(Dropout(0.25))
+    model.add(Dense(N_class, activation='softmax'))
+    model.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.00025), metrics=['accuracy'])
+    print model.summary()
+    return model
+### program start ###
+print 'program start'
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+# let normalize and output to float
+x_train = np.divide(x_train,255.)
+x_test = np.divide(x_test,255.)
+y_train, y_test = y_train.astype('float32'), y_test.astype('float32')
+# set 28x28 to 28x28x1
+x_train = x_train.reshape(x_train.shape[0],x_train[0].shape[0],x_train[0].shape[1],1)
+x_test = x_test.reshape(x_test.shape[0],x_test[0].shape[0],x_test[0].shape[1],1)
+# activate model
+model=model(x_train)
+# fit model
+print y_train[2]
+exit()
+model.fit(x_train, y_train, nb_epoch = 3, batch_size = 128)
+model.save(name_saved_model)
+score = model.evaluate(x_test, y_test, verbose=0)
+print("Accuracy: %.3f%%" % (scores[1]*100))
