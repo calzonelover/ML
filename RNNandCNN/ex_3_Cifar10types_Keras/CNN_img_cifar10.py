2b2cce396a1947376c266e50e55c0dfc47dbae9a
calzonelover <patomporn.pay@gmail.com>
2017-09-25 13:24:50 +0700

    25Sep2017

--- RNNandCNN/ex_3_Cifar10types_Keras/CNN_img_cifar10.py
+++ RNNandCNN/ex_3_Cifar10types_Keras/CNN_img_cifar10.py
@@ -25,7 +25,7 @@ def order_data(f_name):
     return x_test,y_test
 def model(x_train):
     model=Sequential()
-    model.add(Conv2D(32,(3,3),activation='relu',input_shape=(,x_train[0].shape[1],32,1)))
+    model.add(Conv2D(32,(3,3),activation='relu',input_shape=(10000,32,32,3)))
     model.add(MaxPooling2D(pool_size=(3,3)))
     model.add(Conv2D(32,(3,3),activation='relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
@@ -42,7 +42,7 @@ def model(x_train):
 x_train,y_train = order_data('data_batch_1')
 x_test,y_test = order_data('test_batch')
 # initialize the model
-#print x_train[0].shape#.shape,x_train.shape[0],x_train.shape[1],x_train.shape[2]
+print x_train.shape#.shape,x_train.shape[0],x_train.shape[1],x_train.shape[2]
 #exit()
 model=model(x_train)
 # train model
--- Tf_tutorials/Medium_Erik/Ex_1.py
+++ Tf_tutorials/Medium_Erik/Ex_1.py
@@ -1,1 +1,7 @@
 import tensorflow as tf
+
+hello = tf.constant('Hello, tensorflow')
+
+sess = Session()
+
+print sess.run(hello)
