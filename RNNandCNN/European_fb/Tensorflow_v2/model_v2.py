07c6171c2b753a9406ccf0d0e0a75e9916cddc44
calzonelover <patomporn.pay@gmail.com>
2017-11-06 14:28:00 +0700

    6Nov

--- RNNandCNN/European_fb/Tensorflow_v2/model_v2.py
+++ RNNandCNN/European_fb/Tensorflow_v2/model_v2.py
@@ -128,6 +128,60 @@ def model_CNN_RNN_v0(data_quan, data_qual):
     output = tf.nn.softmax(output)
     return output
 
+def model_CNN_v0(data_quan, data_qual):
+    n_hidden = 100
+    n_hidden_2 = 100
+    n_classes = 3
+    hd_layer_out = {'weights': tf.Variable(tf.random_normal([n_hidden, n_classes]))\
+                  ,'biases': tf.Variable(tf.random_normal([n_classes]))}
+    # quantity input
+    x_quan = data_quan
+    # quality word look up
+    x_league = tf.slice(data_qual, [0,0], [-1,1])
+    embed_x_league = tf.nn.embedding_lookup(embed_sp_league, x_league) # shape [None, 1, embed_size]
+    x_season = tf.slice(data_qual, [0,1], [-1,1])
+    embed_x_season = tf.nn.embedding_lookup(embed_sp_season, x_season) # shape [None, 1, embed_size]
+    x_stage = tf.slice(data_qual, [0,2], [-1,1])
+    embed_x_stage = tf.nn.embedding_lookup(embed_sp_stage, x_stage) # shape [None, 1, embed_size]
+    x_team = tf.slice(data_qual, [0,3], [-1,2])
+    embed_x_team = tf.nn.embedding_lookup(embed_sp_team, x_team) # shape [None, 2, embed_size]
+    x_player = tf.slice(data_qual, [0,5], [-1,22])
+    embed_x_player = tf.nn.embedding_lookup(embed_sp_player, x_player) # shape [None, 22, embed_size]
+    # reshape to matrice-tensor to sequence of vector
+    embed_x_league = tf.reshape(embed_x_league, [-1, embed_size])
+    embed_x_season = tf.reshape(embed_x_season, [-1, embed_size])
+    embed_x_stage = tf.reshape(embed_x_stage, [-1, embed_size])
+    embed_x_team = tf.reshape(embed_x_team, [-1, 2*embed_size])
+    embed_x_player = tf.reshape(embed_x_player, [-1, 22*embed_size])
+    # combine every fucking inputs
+    data = tf.concat([x_quan, embed_x_league, embed_x_season\
+                             , embed_x_stage, embed_x_team, embed_x_player], 1)
+    # get size of element in vector
+    data = tf.reshape(data, [-1, 14+(embed_size*27)])  # shape [-1, 68] for embed_size = 2
+    data = tf.reshape(data, [-1, 4, 17, 1]) # shape [4,17,1]
+    # CNN layer
+    conv1 = tf.layers.conv2d(inputs=data,
+                            filters=32,
+                            kernel_size=[3, 3],
+                            padding="same",
+                            activation=tf.nn.relu)
+    conv2 = tf.layers.conv2d(inputs=conv1,
+                             filters=64,
+                             kernel_size=[5, 5],
+                             padding="same",
+                             activation=tf.nn.relu)
+    # Dense Layer
+    flat_dense = tf.contrib.layers.flatten(conv2)
+    dense = tf.layers.dense(inputs=flat_dense, units=1024, activation=tf.nn.relu)
+    dropout = tf.layers.dropout(inputs=dense, rate=0.4)
+    # LSTM layer
+    data = tf.layers.dense(dropout, n_hidden_2)
+    data = tf.reshape(data, [-1, n_hidden_2])
+    # putput
+    output = tf.matmul(data, hd_layer_out['weights']) + hd_layer_out['biases']
+    output = tf.nn.softmax(output)
+    return output
+
 # ===============
 #  Model Score
 # ===============
